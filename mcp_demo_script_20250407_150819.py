c8e5b72c5328119732f417c0910c5d09faa3ed7e
Mohammed Albunde <moh.iq8567@gmail.com>
2025-04-07 15:08:20 -0400

    Add MCP demo script generated on 2025-04-07

---
+++ mcp_demo_script_20250407_150819.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+MCP Demo File
+Generated at 2025-04-07 15:08:19
+"""
+
+def main():
+    print("This is my code using MCP server")
+    
+if __name__ == "__main__":
+    main()
